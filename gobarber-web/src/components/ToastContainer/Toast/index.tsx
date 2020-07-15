552e05d373e3637071d2863fc1fe46241938aaff
GermainPereira <pereira.germain@outlook.com>
2020-07-15 09:33:41 -0300

    Added different styles to toast

--- gobarber-web/src/components/ToastContainer/Toast/index.tsx
+++ gobarber-web/src/components/ToastContainer/Toast/index.tsx
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Container } from './styles';
 
 import { ToastMessage, useToast } from '../../../hooks/toast';
 
-import { FiXCircle, FiAlertCircle } from 'react-icons/fi';
+import {
+  FiXCircle,
+  FiCheckCircle,
+  FiInfo,
+  FiAlertCircle,
+} from 'react-icons/fi';
 
 interface ToastProps {
   message: ToastMessage;
 }
 
+const icons = {
+  info: <FiInfo size={24} />,
+  error: <FiAlertCircle size={24} />,
+  success: <FiCheckCircle size={24} />,
+};
+
 const Toast: React.FC<ToastProps> = ({ message }) => {
   const { removeToast } = useToast();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      removeToast(message.id);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [removeToast, message.id]);
+
   return (
     <Container type={message.type} hasDescription={!!message.description}>
-      <FiAlertCircle size={20} />
+      {icons[message.type || 'info']}
 
       <div>
         <strong>{message.title}</strong>
